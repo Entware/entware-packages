6fd16579a44ce5457350829772760aba5ae9a7a5
Alexander Ryzhov <github@ryzhov-al.ru>
2016-02-17 15:08:15 +0300

    xl2tpd: fix scripts

--- net/xl2tpd/files/ip-down.sh
+++ net/xl2tpd/files/ip-down.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/usr/bin/logger "*** L2TP client disconnected [$*]"
+
+[ ! -z "$(which logger)" ] && logger "*** L2TP client disconnected [$*]"
 iptables -D INPUT -i $1 -j ACCEPT
 iptables -D FORWARD -i $1 -j ACCEPT
 iptables -t nat -D POSTROUTING -o $1 -j MASQUERADE
--- net/xl2tpd/files/ip-up.sh
+++ net/xl2tpd/files/ip-up.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/usr/bin/logger "*** L2TP client connected [$*]"
+
+[ ! -z "$(which logger)" ] && logger "*** L2TP client connected [$*]"
 iptables -I INPUT 1 -i $1 -j ACCEPT
 iptables -I FORWARD 1 -i $1 -j ACCEPT
 iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
