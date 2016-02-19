71441f9bc4f356362011eb9ec2d6740291f7eb97
Alexander Ryzhov <github@ryzhov-al.ru>
2016-02-19 18:57:35 +0300

    xl2tpd: set PATH var

--- net/xl2tpd/files/ip-down.sh
+++ net/xl2tpd/files/ip-down.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/opt/sbin:/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 [ ! -z "$(which logger)" ] && logger "*** L2TP client disconnected [$*]"
 iptables -D INPUT -i $1 -j ACCEPT
 iptables -D FORWARD -i $1 -j ACCEPT
--- net/xl2tpd/files/ip-up.sh
+++ net/xl2tpd/files/ip-up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/opt/sbin:/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 [ ! -z "$(which logger)" ] && logger "*** L2TP client connected [$*]"
 iptables -I INPUT 1 -i $1 -j ACCEPT
 iptables -I FORWARD 1 -i $1 -j ACCEPT
