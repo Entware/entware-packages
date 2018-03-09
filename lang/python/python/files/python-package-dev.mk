4f496b49164f84f5f23e9d8127201c65c6c7bb4d
zyxmon <zyxmon@gmail.com>
2018-03-09 21:25:17 +0300

    python-dev: remove *.so, they are provided by python-base

--- lang/python/python/files/python-package-dev.mk
+++ lang/python/python/files/python-package-dev.mk
@@ -15,7 +15,6 @@ define PyPackage/python-dev/install
 	$(INSTALL_DIR) $(1)/opt/bin $(1)/opt/lib
 	$(CP) $(PKG_INSTALL_DIR)/opt/bin/python*config $(1)/opt/bin
 	$(CP) $(PKG_INSTALL_DIR)/opt/lib/python$(PYTHON_VERSION)/config/libpython$(PYTHON_VERSION).a $(1)/opt/lib
-	$(CP) $(PKG_INSTALL_DIR)/opt/lib/libpython$(PYTHON_VERSION).so* $(1)/opt/lib
 	$(SED) "s|-L$(STAGING_DIR)/opt/lib|-L/opt/lib|g" $(PKG_INSTALL_DIR)/opt/lib/python$(PYTHON_VERSION)/config/Makefile
 	$(SED) "s|-L$(STAGING_DIR)/lib|-L/opt/lib/gcc/$(REAL_GNU_TARGET_NAME)/6.3.0|g" $(PKG_INSTALL_DIR)/opt/lib/python$(PYTHON_VERSION)/config/Makefile
 	$(SED) "s|-L$(TOOLCHAIN_DIR)/usr/lib||g" $(PKG_INSTALL_DIR)/opt/lib/python$(PYTHON_VERSION)/config/Makefile
