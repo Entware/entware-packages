41db4e458fe43d5b6b932ae4c99f368fbe982e10
The-BB <tun.chen.bo@gmail.com>
2019-02-27 12:49:09 +0300

    python3: sync with openwrt, 2019.02

--- lang/python/python3/files/python3-package-pip.mk
+++ lang/python/python3/files/python3-package-pip.mk
@@ -8,19 +8,21 @@
 define Package/python3-pip
 $(call Package/python3/Default)
   TITLE:=Python $(PYTHON3_VERSION) pip module
-  VERSION:=$(PYTHON3_PIP_VERSION)
+  VERSION:=$(PYTHON3_PIP_VERSION)-$(PYTHON3_PIP_PKG_RELEASE)
   DEPENDS:=+python3 +python3-setuptools +python-pip-conf
 endef
 
 define Package/python3-pip/install
 	$(INSTALL_DIR) $(1)/opt/bin $(1)/opt/lib/python$(PYTHON3_VERSION)/site-packages
-	# Adjust shebang to proper python location on target
-	sed "1s@.*@#\!/opt/bin/python$(PYTHON3_VERSION)@" -i $(PKG_BUILD_DIR)/install-pip/bin/*
 	$(CP) $(PKG_BUILD_DIR)/install-pip/bin/pip3* $(1)/opt/bin
 	$(CP) \
 		$(PKG_BUILD_DIR)/install-pip/lib/python$(PYTHON3_VERSION)/site-packages/pip \
+		$(PKG_BUILD_DIR)/install-pip/lib/python$(PYTHON3_VERSION)/site-packages/pip-$(PYTHON3_PIP_VERSION).dist-info \
 		$(1)/opt/lib/python$(PYTHON3_VERSION)/site-packages/
-	find $(1)/opt/lib/python$(PYTHON3_VERSION)/site-packages/ -name __pycache__ | xargs rm -rf
+	for _ in \$(seq 1 10) ; do \
+		find $(1)/opt/lib/python$(PYTHON3_VERSION)/site-packages/ -name __pycache__ -exec rm -rf {} \; || continue ; \
+		break ; \
+	done
 endef
 
 $(eval $(call Py3BasePackage,python3-pip, \
