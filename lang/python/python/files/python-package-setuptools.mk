e78b25b1c61831798251ec2b819fcd2a31ab25bc
The-BB <tun.chen.bo@gmail.com>
2019-02-27 12:48:56 +0300

    python: sync with openwrt, 2019.02

--- lang/python/python/files/python-package-setuptools.mk
+++ lang/python/python/files/python-package-setuptools.mk
@@ -8,18 +8,17 @@
 define Package/python-setuptools
 $(call Package/python/Default)
   TITLE:=Python $(PYTHON_VERSION) setuptools module
-  VERSION:=$(PYTHON_SETUPTOOLS_VERSION)
+  VERSION:=$(PYTHON_SETUPTOOLS_VERSION)-$(PYTHON_SETUPTOOLS_PKG_RELEASE)
   DEPENDS:=+python
 endef
 
 define PyPackage/python-setuptools/install
 	$(INSTALL_DIR) $(1)/opt/bin $(1)/opt/lib/python$(PYTHON_VERSION)/site-packages
-	# Adjust shebang to proper python location on target
-	sed "1s@.*@#\!/opt/bin/python$(PYTHON_VERSION)@" -i $(PKG_BUILD_DIR)/install-setuptools/bin/*
 	$(CP) $(PKG_BUILD_DIR)/install-setuptools/bin/* $(1)/opt/bin
 	$(CP) \
 		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON_VERSION)/site-packages/pkg_resources \
 		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON_VERSION)/site-packages/setuptools \
+		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON_VERSION)/site-packages/setuptools-$(PYTHON_SETUPTOOLS_VERSION).dist-info \
 		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON_VERSION)/site-packages/easy_install.py \
 		$(1)/opt/lib/python$(PYTHON_VERSION)/site-packages
 endef
