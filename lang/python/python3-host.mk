dfc7d953aac814ab466bf311b3f3f339d3964005
The-BB <tun.chen.bo@gmail.com>
2019-05-16 21:22:30 +0300

    lang/python/python3-host.mk: sync with openwrt, 2019.05

--- lang/python/python3-host.mk
+++ lang/python/python3-host.mk
@@ -33,8 +33,8 @@ define HostPython3
 		export PYTHONPATH="$(PYTHON3PATH)"; \
 		export PYTHONDONTWRITEBYTECODE=1; \
 		export _python_sysroot="$(STAGING_DIR)"; \
-		export _python_prefix="/usr"; \
-		export _python_exec_prefix="/usr"; \
+		export _python_prefix="/opt"; \
+		export _python_exec_prefix="/opt"; \
 	fi; \
 	export PYTHONOPTIMIZE=""; \
 	$(1) \
