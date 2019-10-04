e1fa8bf3ceb71da5fb069b1d78d7e8994599776f
Alexander Ryzhov <github@ryzhov-al.ru>
2019-10-04 17:45:46 +0300

    scons: explicit use of our own python3

--- devel/scons/files/pywrap.sh
+++ devel/scons/files/pywrap.sh
@@ -5,7 +5,7 @@ case "${0##*/}" in
 	*) arg1="$0.py" ;;
 esac
 
-for bin in python python3; do
+for bin in python3; do
     case "$($bin -V 2>&1)" in
         "Python 3"*) exec $bin $arg1 "$@" ;;
     esac
