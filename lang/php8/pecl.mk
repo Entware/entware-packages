9081b74e5537d7e9591e27d3a2d98241272f48a5
The-BB <tun.chen.bo@gmail.com>
2021-07-05 18:05:03 +0300

    lang/php8: sync with OpenWrt, 2021.07

--- lang/php8/pecl.mk
+++ lang/php8/pecl.mk
@@ -13,11 +13,11 @@ endef
 
 define Build/Prepare
 	$(Build/Prepare/Default)
-	$(if $(QUILT),,( cd $(PKG_BUILD_DIR); $(STAGING_DIR)/usr/bin/phpize8 ))
+	$(if $(QUILT),,( cd $(PKG_BUILD_DIR); $(STAGING_DIR)/opt/bin/phpize8 ))
 endef
 
 define Build/Configure
-	$(if $(QUILT),( cd $(PKG_BUILD_DIR); $(STAGING_DIR)/usr/bin/phpize8 ))
+	$(if $(QUILT),( cd $(PKG_BUILD_DIR); $(STAGING_DIR)/opt/bin/phpize8 ))
 	$(Build/Configure/Default)
 endef
 
@@ -25,7 +25,7 @@ CONFIGURE_VARS+= \
 	ac_cv_c_bigendian_php=$(if $(CONFIG_BIG_ENDIAN),yes,no)
 
 CONFIGURE_ARGS+= \
-	--with-php-config=$(STAGING_DIR)/usr/bin/php8-config
+	--with-php-config=$(STAGING_DIR)/opt/bin/php8-config
 
 define PHP8PECLPackage
 
@@ -41,13 +41,13 @@ define PHP8PECLPackage
   endef
 
   define Package/php8-pecl-$(1)/install
-	$(INSTALL_DIR) $$(1)/usr/lib/php8
-	$(INSTALL_BIN) $(PKG_BUILD_DIR)/modules/$(subst -,_,$(1)).so $$(1)/usr/lib/php8/
-	$(INSTALL_DIR) $$(1)/etc/php8
+	$(INSTALL_DIR) $$(1)/opt/lib/php8
+	$(INSTALL_BIN) $(PKG_BUILD_DIR)/modules/$(subst -,_,$(1)).so $$(1)/opt/lib/php8/
+	$(INSTALL_DIR) $$(1)/opt/etc/php8
     ifeq ($(5),zend)
-	echo "zend_extension=/usr/lib/php8/$(subst -,_,$(1)).so" > $$(1)/etc/php8/$(if $(4),$(4),20)_$(subst -,_,$(1)).ini
+	echo "zend_extension=/opt/lib/php8/$(subst -,_,$(1)).so" > $$(1)/opt/etc/php8/$(if $(4),$(4),20)_$(subst -,_,$(1)).ini
     else
-	echo "extension=$(subst -,_,$(1)).so" > $$(1)/etc/php8/$(if $(4),$(4),20)_$(subst -,_,$(1)).ini
+	echo "extension=$(subst -,_,$(1)).so" > $$(1)/opt/etc/php8/$(if $(4),$(4),20)_$(subst -,_,$(1)).ini
     endif
   endef
 
